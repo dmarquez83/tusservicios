12435b054471da1c6b75fd069b86633e9d20c7b2
carolina <cmazzagliar@gmail.com>
2015-11-11 22:41:21 -0430

    diseño de categorias

--- resources/views/categorias/table.blade.php
+++ resources/views/categorias/table.blade.php
@@ -1,3 +1,14 @@
+<div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="input-group">
+        <div class="input-group-addon">
+            <span class="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>
+        </div>
+        <input type="text" class="form-control" id="exampleInputAmount" placeholder="Encuentra el Servicio que necesitas">
+    </div>
+</div>
+<br>
+<br>
+
 @foreach($categorias as $categoria)
 
             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center">
