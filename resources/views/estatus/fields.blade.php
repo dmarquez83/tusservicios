f23fd4ccfc8530bae5fcc09c8cccd8766b4ccf78
carolina <cmazzagliar@gmail.com>
2015-11-17 15:12:58 -0430

    cambios en status y ponderacions

--- resources/views/estatus/fields.blade.php
+++ resources/views/estatus/fields.blade.php
@@ -1,41 +1,49 @@
-<!--- Nombre Field --->
-<div class="row">
-    <div class="input-field">
-        {!! Form::label('nombre', 'Nombre:') !!}
-        {!! Form::text('nombre', null, ['class' => 'form-control']) !!}
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="box box-warning">
+        <div class="box-header">
+            <h3 class="box-title">Editar Ponderacions</h3>
+        </div>
+        <div class="box-body">
+            <div class="row">
+                <div class="col-sm-6 form-group">
+                    {!! Form::label('nombre', 'Nombre:', ['class' => 'control-label']) !!}
+                </div>
+                <div class="col-sm-6 form-group">
+                    {!! Form::label('descripcion', 'Descripcion:', ['class' => 'control-label']) !!}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 form-group">
+                    {!! Form::text('nombre', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="col-sm-6 form-group">
+                    {!! Form::textarea('descripcion', null, ['class' => 'form-control','rows' =>'3']) !!}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6 form-group">
+                    {!! Form::label('tabla', 'Tabla:', ['class' => 'control-label']) !!}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 form-group">
+                    {!! Form::select('tabla', array(
+                       'solicitud' => 'Solicitud',
+                       'servicios' => 'Servicios',
+                       'postulados' => 'Postulados',
+                       'servicios' => 'Servicios',
+                       'catalogos' => 'Catalogos',
+                       'catalogos_insumos' => 'Catalogos Insumos',
+                   ), null, []) !!}
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-right">
+                    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-
-
-
-<!--- Descripcion Field --->
-<div class="row">
-    <div class="input-field">
-    {!! Form::label('descripcion', 'Descripcion:') !!}
-	{!! Form::textarea('descripcion', null, ['class' => 'materialize-textarea']) !!}
-    </div>
-</div>
-
-<!--- Tabla Field --->
-<div class="row">
-    <div class="input-field">
-    {!! Form::label('tabla', 'Tabla:') !!}
-
-    {!! Form::select('tabla', array(
-    'solicitud' => 'Solicitud',
-    'servicios' => 'Servicios',
-    'postulados' => 'Postulados',
-    'servicios' => 'Servicios',
-    'catalogos' => 'Catalogos',
-    'catalogos_insumos' => 'Catalogos Insumos',
-), null, []) !!}
-</div>
-</div>
-
-
-
-<!--- Submit Field --->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
 </div>
--- resources/views/ponderacions/table.blade.php
+++ resources/views/ponderacions/table.blade.php
@@ -1,19 +1,32 @@
-<table class="table">
-    <thead>
-    <th>Valor</th>
-			<th>Nombre</th>
-    <th width="50px">Action</th>
-    </thead>
-    <tbody>
-    @foreach($ponderacions as $ponderacion)
-        <tr>
-            <td>{!! $ponderacion->valor !!}</td>
-			<td>{!! $ponderacion->nombre !!}</td>
-            <td>
-                <a href="{!! route('ponderacions.edit', [$ponderacion->id]) !!}"><i class="mdi-content-send"></i></a>
-                <a href="{!! route('ponderacions.delete', [$ponderacion->id]) !!}" onclick="return confirm('Are you sure wants to delete this Ponderacion?')"><i class="mdi-action-delete"></i></a>
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+<div class="col-xs-2">
+</div>
+<div class="col-xs-8">
+    <div class="box box-warning">
+        <div class="box-header">
+            <h3>Ponderacions</h3>
+        </div>
+    <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+            <tbody>
+                <tr>
+                    <th style="width: 30px">Valor</th>
+                    <th style="width: 60px">Nombre</th>
+                    <th style="width: 10px" class="text-right">Action</th>
+                </tr>
+                @foreach($ponderacions as $ponderacion)
+                    <tr>
+                        <th>{!! $ponderacion->valor !!}</th>
+                        <th>{!! $ponderacion->nombre !!}</th>
+                        <th class="text-right">
+                            <a href="{!! route('ponderacions.edit', [$ponderacion->id]) !!}"><i class="glyphicon glyphicon-pencil"></i></a>
+                            <a href="{!! route('ponderacions.delete', [$ponderacion->id]) !!}" onclick="return confirm('Are you sure wants to delete this Ponderacion?')"><i class="glyphicon glyphicon-remove"></i></a>
+                        </th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="col-xs-2">
+
+</div>
