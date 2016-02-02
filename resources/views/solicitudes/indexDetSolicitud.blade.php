94f4280eabeecd6cddbcfa4d669351eab28eb877
carolina <cmazzagliar@gmail.com>
2016-02-02 12:13:30 -0430

    ruta de imagenes

--- resources/views/solicitudes/indexDetSolicitud.blade.php
+++ resources/views/solicitudes/indexDetSolicitud.blade.php
@@ -5,3 +5,33 @@
         @include('solicitudes.detSolicitud')
     </div>
 @endsection
+
+@section('scripts')
+    <script type="text/javascript">
+        $(function(){
+            var costo=parseFloat($('#costo').data('costo'));
+            var total=costo;
+            $("#total").text(total);
+            $('input:checkbox').removeAttr('checked');
+            var check = $('.boton_check');
+            check.click(function(){
+                var valor=parseFloat($(this).data('valor'));
+                if ($(this).prop('checked')){
+                    sumar(valor)
+                }
+                else {
+                    restar(valor);
+                }
+            })
+
+            function sumar(valor) {
+                total+=valor;
+                $("#total").text(total);
+            }
+            function restar(valor) {
+                total-=valor;
+                $("#total").text(total);
+            }
+        })
+    </script>
+@endsection
